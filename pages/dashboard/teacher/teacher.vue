4daab6090cb5724858106c369fa11054a2e52408
louis <50340099@qq.com>
2025-03-12 16:43:25 +0800

    老师总览页面

--- pages/dashboard/teacher/teacher.vue
+++ pages/dashboard/teacher/teacher.vue
@@ -2,17 +2,28 @@
     <view class="dashboard">
         <custom-nav :xcxName="'成长评估'" :navCustomStyle="navCustomStyle" :needBar="false" />
         <view class="user-profile">
-            <image class="avatar-image" :src="avatarUrl" />
+            <!-- 左侧内容容器 -->
 
-            <view class="info">
-                <view class="name">李小思爸爸</view>
-                <view class="class">小班3班 <view class="invite">邀请加入本班</view>
+            <view class="profile-left">
+                <image class="avatar-image" :src="avatarUrl" />
+                <view class="info">
+                    <view class="name">李老师</view>
+                    <view class="class">小班3班 <view class="invite">邀请加入本班</view>
+                    </view>
                 </view>
             </view>
-            <image class="switch-class-image" :src="switchIconUrl"></image>
 
+            <!-- 右侧切换按钮 -->
+            <view class="switch-class">
+                <image class="switch-class-image" :src="switchIconUrl"></image>
+            </view>
         </view>
-
+        <view class="assessment-option">
+            <view class="option" @click="clickAssessmentItem">
+                <view class="title">{{ $t('assessmentList.first') }}</view>
+                <image class="image" src="../../../static/assessment-list/child-assess.svg" />
+            </view>
+        </view>
 
     </view>
 </template>
@@ -47,21 +58,115 @@ const reports = [
 <style lang="scss" scoped>
 .dashboard {
     .user-profile {
-        height: calc(100vh / 3);
-        background-color: red;
+        height: calc(100vh / 8);
+        background:
+            linear-gradient(to right, #F5FDF8, #F1FCF5, #F9FCEF);
         display: flex;
-        padding-left: 40rpx;
-        padding-right: 40rpx;
+        justify-content: space-between;
+        padding: 0 40rpx;
+        box-shadow: inset 0 -20rpx 30rpx rgba(255, 255, 255, 0.8);
+
+        .profile-left {
+            display: flex;
+            gap: 24rpx;
+            height: 60%;
+            align-items: center;
+        }
 
         .avatar-image {
             width: 120rpx;
             height: 120rpx;
         }
 
-        .switch-class-image {
-            width: 60rpx;
-            height: 60rpx;
+        .switch-class {
+            display: flex;
+            height: 60%;
+            align-items: center;
+
+            .switch-class-image {
+                width: 90rpx;
+                height: 90rpx;
+            }
         }
+
+
+        .info {
+            display: flex;
+            flex-direction: column;
+            gap: 8rpx;
+
+            .name {
+                color: #00214D;
+                font-family: "PingFang SC";
+                font-size: 18px;
+                font-style: normal;
+                font-weight: 600;
+                line-height: 24px;
+            }
+
+            .class {
+                display: flex;
+                gap: 8rpx;
+                color: #3D464A;
+                font-family: "PingFang SC";
+                font-size: 14px;
+                font-style: normal;
+                font-weight: 400;
+                line-height: 20px;
+                align-items: center;
+
+                .invite {
+                    background: #DBE9FF;
+                    color: #2A64E9;
+                    padding: 4px 4px;
+                    border-radius: 4px;
+                    font-family: "PingFang SC";
+                    font-size: 14px;
+                    font-style: normal;
+                    line-height: 20px;
+                    margin-left: 8px;
+                }
+            }
+        }
+    }
+
+    .assessment-option {
+        margin: 0 40rpx 0 40rpx;
+        border-radius: 12px;
+
+        .option {
+            border: 1px solid #100D40;
+            width: calc(100vw - 80rpx);
+            height: 118px;
+            flex-shrink: 0;
+            border-radius: 12px;
+            background: #FFF;
+            box-shadow: 0px 0px 1px 0px rgba(193, 197, 210, 0.20);
+            margin-bottom: 32rpx;
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+
+            .title {
+                color: #100D40;
+                font-family: "PingFang SC";
+                font-size: 20px;
+                font-style: normal;
+                font-weight: 600;
+                line-height: normal;
+                margin-left: 80rpx;
+                margin-right: 90rpx;
+            }
+
+            .image {
+                width: 170rpx;
+                height: 170rpx;
+                margin-right: 40rpx;
+            }
+        }
+
+
+
     }
 }
 
