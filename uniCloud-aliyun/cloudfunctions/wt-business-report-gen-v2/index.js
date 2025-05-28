865f87a01cad3cf0bb3e8b2a63035cb56df3c95b
louis <50340099@qq.com>
2025-05-28 14:09:31 +0800

    报告总结

--- uniCloud-aliyun/cloudfunctions/wt-business-report-gen-v2/index.js
+++ uniCloud-aliyun/cloudfunctions/wt-business-report-gen-v2/index.js
@@ -111,41 +111,33 @@ exports.main = async (event, context) => {
 				let skillBelowStandard = []
 				let subsectionsSummary = []
 				if (completedSectionList.length > 0) {
-					completedSectionList.forEach(async (item) => {
+					for (const item of completedSectionList) {
 						const { recordId, assessmentId, assessorId, assessorName,
 							classId, className, childId, avatar, childName, childAge,
-							ageInt, assessmentTitle } = item
+							ageInt, assessmentTitle } = item;
+
 						report = {
 							reportVersion: 'v2',
 							reportId: `report_ablls_${childId}_${Date.now()}`,
 							recordId, assessmentId, assessorId, assessorName,
 							classId, className, childId, avatar, childName, childAge,
 							ageInt, assessmentTitle
-						}
-						let abllsSectionSummaryList = []
-						let abllsSectionSummary = {}
-						item.assessmentRecords.forEach(async (record) => {
-							let abllsSectionName = record.sectioName
-							let abllsSectionAlphabet = record.alphabet
-							let expectedTotalScore = record.expectedTotalScore
-							let actualTotalScore = record.actualTotalScore
-							let skillBelowStandard = []
+						};
 
-							// console.log("completedSectionList item record", record)
-							// 找record里questions里的isStandard为false的
-							record.questions.forEach(async (question) => {
-								// console.log("completedSectionList item record question", question.isStandard)
+						const abllsSectionSummaryList = [];
 
-								let temp = {}
-								// 没达到标准的 
+						for (const record of item.assessmentRecords) {
+							const abllsSectionSummary = {
+								abllsSectionName: record.sectioName,
+								abllsSectionAlphabet: record.alphabet,
+								expectedTotalScore: record.expectedTotalScore,
+								actualTotalScore: record.actualTotalScore,
+								skillBelowStandard: []
+							};
+
+							for (const question of record.questions) {
 								if (!question.isStandard) {
-									abllsSectionSummary = {
-										abllsSectionName: abllsSectionName,
-										abllsSectionAlphabet: abllsSectionAlphabet,
-										expectedTotalScore: expectedTotalScore,
-										actualTotalScore: actualTotalScore,
-									}
-									temp = {
+									const temp = {
 										taskName: question.task_name,
 										taskObject: question.task_object,
 										taskContent: question.content,
@@ -153,7 +145,6 @@ exports.main = async (event, context) => {
 										actualScore: question.score,
 										expectedOutcome: (() => {
 											const ageStandard = question.age_standards?.find(as => as.age === item.ageInt);
-											// console.log("ageStandard", ageStandard)
 											if (ageStandard) {
 												const matchingOption = question.options.find(opt => opt.score === ageStandard.expected_score);
 												return matchingOption?.name || '';
@@ -161,30 +152,27 @@ exports.main = async (event, context) => {
 											return '';
 										})(),
 										expectedScore: question.expected_score
-									}
-									if (temp) {
-										skillBelowStandard.push(temp)
-										abllsSectionSummary.skillBelowStandard = skillBelowStandard
-									}
+									};
+									abllsSectionSummary.skillBelowStandard.push(temp);
 								}
-							})
-							// subsectionsSummary.push(abllsSectionSummary)
-							if (Object.keys(abllsSectionSummary).length > 0) {
-								// subsectionsSummary.push(abllsSectionSummary)
-								// console.log("abllsSectionSummary", abllsSectionSummary)
-								abllsSectionSummaryList.push(abllsSectionSummary)
-								// console.log("abllsSectionSummaryList", abllsSectionSummaryList)
 							}
-						})
-						sectionSummary.sectionName = item.sectionName
-						sectionSummary.sectionId = item.sectionId
-						sectionSummary.abllsSectionSummaryList = abllsSectionSummaryList
-						// console.log("sectionSummary", sectionSummary)
-						console.log("sectionSummary", sectionSummary)
 
-					})
+							if (abllsSectionSummary.skillBelowStandard.length > 0) {
+								abllsSectionSummaryList.push(abllsSectionSummary);
+							}
+						}
 
+						const sectionSummary = {
+							sectionName: item.sectionName,
+							sectionId: item.sectionId,
+							abllsSectionSummaryList
+						};
+
+						sectionSummaryList.push(sectionSummary);
+					}
 				}
+
+
 				// console.log("report", report)
 				// console.log("subsectionsSummary", subsectionsSummary)
 				// 去掉空的对象
@@ -193,7 +181,7 @@ exports.main = async (event, context) => {
 
 				return {
 					code: 200,
-					data: { result, report },
+					data: { result, report: { ...report, sectionSummaryList } },
 					message: '报告生成中，请稍后查询'
 				};
 			} else {
